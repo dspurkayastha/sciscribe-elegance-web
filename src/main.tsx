5d88324eadfef7eb0ebac716b0175117537e3c53
dspurkayastha <dspurkayastha@icloud.com>
2025-05-08 21:35:15 +0530

    firebase integration

--- src/main.tsx
+++ src/main.tsx
@@ -2,4 +2,9 @@ import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
+// Initialize Firebase (browser only)
+if (typeof window !== 'undefined') {
+  import('./lib/firebase');
+}
+
 createRoot(document.getElementById("root")!).render(<App />);
