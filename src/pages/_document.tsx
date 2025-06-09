3cd00d6141f3002d45d1344e7db5d68976e551f1
dspurkayastha <dspurkayastha@icloud.com>
2025-06-09 16:01:38 +0530

    Add CTA button for WhatsApp contact

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -15,13 +15,13 @@ export const Document: React.FC<DocumentProps> = ({ children, styles }) => {
         <meta name="theme-color" content="#6366F1" />
         
         {/* Google tag (gtag.js) */}
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-MVG24STSTN"></script>
+        // <script async src="https://www.googletagmanager.com/gtag/js?id=G-MVG24STSTN" />
+        // <script dangerouslySetInnerHTML={{ __html: `gtag('config', 'G-MVG24STSTN')` }} />
         <script>
           {`
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
-            gtag('config', 'G-MVG24STSTN');
           `}
         </script>
 
